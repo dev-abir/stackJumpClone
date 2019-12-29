b3a2b0756fe6ecb9fd55f68535cde37c56bf4179
dev-abir <gangulyabir777@gmail.com>
2019-12-29 14:45:57 +0530

    rearrangement... right now nothing is going to work..

---
+++ src/game/GameSettings.h
@@ -0,0 +1,43 @@
+/*
+ * Common.h
+ *
+ *  Created on: 07-Oct-2019
+ *      Author: pluto
+ */
+
+#ifndef MISC_COMMON_H_
+#define MISC_COMMON_H_
+
+#define WIDTH 600
+#define HEIGHT 600
+
+///////Physics///////
+#define g 9.8 //In metre per second square
+
+
+
+///////Fonts///////
+#define SERIF_FONT_FILE "resources/fonts/FreeSerif.ttf"
+
+
+
+///////Main Menu///////
+#define MAIN_MENU_FONT_FILE SERIF_FONT_FILE
+#define MAIN_MENU_BACKGROUND_TEXTURE_FILE "resources/sky1.png"
+
+
+
+///////New Game///////
+#define NEW_GAME_BACKGROUND_TEXTURE_FILE "resources/island.png"
+#define NEW_GAME_PLAYER_TEXTURE_FILE "resources/slime.png"
+#define NEW_GAME_PLAYER_TEXTURE_WIDTH 49
+#define NEW_GAME_PLAYER_TEXTURE_HEIGHT 47
+#define MASS_OF_PLAYER 2 //In kg
+#define FORCE_IMPARTED_ON_PRESSING_KEY_UP 2000
+
+
+
+
+
+
+#endif /* MISC_COMMON_H_ */
